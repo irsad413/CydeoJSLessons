594e1681b391022e8f1af7d6fbe7062baa204879
irsadartus <erxat413@gmail.com>
2023-06-20 23:35:10 +0300

    JS_Variables

---
+++ 01_Variables.js
@@ -0,0 +1,46 @@
+console.log("hello");
+
+// Single comment line
+
+/**
+ * 
+ * Multiple comment line
+ */
+
+
+// decalaring bariables 
+
+/**
+ * 
+ * JS is dynamic tuping language
+ */
+
+let a  ;
+console.log(typeof a);
+
+a = 5 ; 
+console.log(typeof a);
+
+a = 'BootCamp' ; 
+console.log(typeof a);
+
+a = true ; 
+console.log(typeof a);
+
+
+let x = 5 ; y = 'Irsad' ; 
+console.log(x+y); // concatination : 5Irsad
+
+console.log(x*y); // NaN
+
+// Naming rules:  same with Java naming rules
+
+//constant variables 
+
+const number = 35 ; 
+// type error: Assignment to constant variable
+// number = 45
+number = 45 ; 
+console.log(number);
+
+
