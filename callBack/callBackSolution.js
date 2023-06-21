dfa623ceb3d205607498996013496d8cab8a2fb5
irsadartus <erxat413@gmail.com>
2023-06-21 18:36:15 +0300

    JS_CallBack

---
+++ callBack/callBackSolution.js
@@ -0,0 +1,32 @@
+function waitingForSomeServerToAct(myCallback) {
+
+    let dataNeeded ; 
+
+    setTimeout(() => {
+
+        console.log("BackEnd Information have just been received");
+        dataNeeded = "Cydeo" ; 
+        myCallback(dataNeeded)
+        
+    }, 5000);
+    
+    return dataNeeded ; 
+}
+
+
+function functionThatNeedsBackEndData (info) {
+
+    console.log(" I need to use BackEnd Data " + info);
+}
+
+
+waitingForSomeServerToAct(functionThatNeedsBackEndData)
+
+
+/**
+ * the first solution JS developers came up with was this callBack situation
+ * ES6 -- promises handles async nature of JS
+ * 2017 --- async function , await keyword\
+ * what is a callBack Hell ? 
+ * what is async code what is sync code? 
+ */
