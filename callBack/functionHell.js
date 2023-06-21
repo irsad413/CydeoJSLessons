8361cafbca2c9a7d2d9a5c7ce3ed7101a0de7d7c
irsadartus <erxat413@gmail.com>
2023-06-21 18:30:02 +0300

    JS_CallBack

---
+++ callBack/functionHell.js
@@ -0,0 +1,43 @@
+/***
+ * in Java: make Database connection and get some data , then you use this data inside your code
+ *  --- Connect DB
+ *  --- gets data
+ *  --- is waiting until the data is received
+ *  --- then executes rest of the code
+ * 
+ * in JS:
+ * 
+ *  --- while trying to connect DB
+ *  --- the rest of the functions does not wait -- async code
+ */
+
+
+
+function waitingForSomeServerToAct() {
+
+    let dataNeeded ; 
+
+    setTimeout(() => {
+
+        console.log("BackEnd Information have just been received");
+        dataNeeded = "Cydeo" ; 
+        console.log(dataNeeded);
+        
+    }, 5000);
+    
+    return dataNeeded ; 
+}
+
+
+function functionThatNeedsBackEndData (info) {
+
+    console.log(" I need to use BackEnd Data " + info);
+}
+
+let data = waitingForSomeServerToAct() ; // calling server connection , getting the data and storing it to a variable
+
+functionThatNeedsBackEndData(data) ; 
+
+
+
+
